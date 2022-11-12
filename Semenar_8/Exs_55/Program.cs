999b98ba2ead491a9cbb3755853fcc6b8c771b2e
Aleksey <alessioevils@gmail.com>
2022-11-12 22:53:10 +0300

    add Seminar 8

--- Semenar_8/Exs_55/Program.cs
+++ Semenar_8/Exs_55/Program.cs
@@ -1,2 +1,61 @@
 ﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это
 // невозможно, программа должна вывести сообщение дляпользователя.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeRowToColunms(int[,] userArray)
+{
+    if (userArray.GetLength(0) == userArray.GetLength(1))
+    {
+        int[,] resultArray = new int[userArray.GetLength(0), userArray.GetLength(1)];
+        for (int i = 0; i < userArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < userArray.GetLength(1); j++)
+            {
+                resultArray[i, j] = userArray[j, i];
+            }
+        }
+        return resultArray;
+    }
+    else
+    {
+        Console.Write("Первернуть нельзя!");
+        return userArray;
+    }
+
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, columns, 0, 9);
+PrintArray(array);
+Console.WriteLine();
+int[,] res = ChangeRowToColunms(array);
+PrintArray(res);
--- Semenar_8/Exs_57/Program.cs
+++ Semenar_8/Exs_57/Program.cs
@@ -1,6 +1,5 @@
 ﻿// Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит
-// информацию о том, сколько раз встречается элемент входных данных.
-// Набор данных Частотный массив
+// информацию о том, сколько раз встречается элемент входных данных. Набор данных Частотный массив:
 
 // { 1, 9, 9, 0, 2, 8, 0, 9 } 0 встречается 2 раза
 // 1 встречается 1 раз
@@ -16,3 +15,83 @@
 // 3 встречается 1 раз
 // 4 встречается 1 раз
 // 6 встречается 2 раза
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+int[] SingleArray(int[,] BiArray)
+{
+    int[] result = new int[BiArray.GetLength(0) * BiArray.GetLength(1)]; // кол-во эл-тов в массиве=кол-ву всех эл-отв
+    int k = 0;
+    for (int i = 0; i < BiArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < BiArray.GetLength(1); j++)
+        {
+            result[k++] = BiArray[i, j];
+        }
+    }
+    return result;
+}
+void SortArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        for (int j = i + 1; j < inArray.Length; j++)
+        {
+            if (inArray[i] > inArray[j])
+            {
+                int k = inArray[i];
+                inArray[i] = inArray[j];
+                inArray[j] = k;
+            }
+        }
+    }
+}
+void CountFindNumberInArray(int[] inarray)
+{
+    int count = 1;
+    int numberZero = inarray[0];
+    for (int i = 1; i < inarray.Length; i++)
+    {
+        if (inarray[i] == numberZero)
+            count++;
+        else
+        {
+            Console.WriteLine($"число {numberZero} встречается {count} раз");
+            numberZero = inarray[i];
+            count = 1;
+        }
+    }
+}
+
+Console.Write("Введите количество строк в массиве:");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве:");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] userArray = GetArray(row, col, 0, 10);
+PrintArray(userArray);
+
+int[] array1D = SingleArray(userArray);
+SortArray(array1D);
+CountFindNumberInArray(array1D);
