03a3d1130b1a9c53479d2adefd95b04cab5ee34d
Aleksey <alessioevils@gmail.com>
2022-11-15 17:32:58 +0300

    add more exs for HomeWork

--- HOME_8/Exs_54/Program.cs
+++ HOME_8/Exs_54/Program.cs
@@ -9,3 +9,58 @@
 // 9 5 3 2
 // 8 4 4 2
 
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве:");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве:");
+int columns = int.Parse(Console.ReadLine()!);
+int[,] array =  GetArray(rows, columns, 0, 9);
+Console.WriteLine("Изначальный массив: ");
+Console.WriteLine();
+PrintArray(array);
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < array.GetLength(1) - 1; z++)
+        {
+            if (array[i, z] < array[i, z + 1]) 
+            {
+                int temp = 0;
+                temp = array[i, z];
+                array[i, z] = array[i, z + 1];
+                array[i, z + 1] = temp;
+            }
+        }
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Упорядоченный по убыванию массив: ");
+Console.WriteLine();
+PrintArray(array);
--- HOME_8/Exs_58/Program.cs
+++ HOME_8/Exs_58/Program.cs
@@ -5,3 +5,54 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размерность матрицы:");
+int matrixSize = int.Parse(Console.ReadLine()!);
+int[,] matrixA = GetArray(matrixSize, matrixSize, 0, 9);
+int[,] matrixB = GetArray(matrixSize, matrixSize, 0, 9);
+
+int[,] matrixC = new int[matrixSize, matrixSize];
+for (int i = 0; i <matrixSize; i++)
+{
+    for (int j = 0; j < matrixSize; j++)
+    {
+        for (int k = 0; k < matrixSize; k++)
+        {
+            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+        }
+    }
+}
+
+Console.WriteLine("Первая матрица: ");
+PrintArray(matrixA);
+Console.WriteLine();
+Console.WriteLine("Вторая матрица: ");
+PrintArray(matrixB);
+Console.WriteLine();
+Console.WriteLine("Произведение матриц: ");
+PrintArray(matrixC);
